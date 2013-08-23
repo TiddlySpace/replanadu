d8894827d7023a5a087eac08cf29c78d67ff84d0
Craig Cook <boycook@me.com>
2013-08-23 13:30:07 +0100

    Adding button to DOM with count

--- assets/twiddlerscount.js
+++ assets/twiddlerscount.js
@@ -1,11 +1,33 @@
 
 function TwiddlersCount() {
 	this.baseUrl = '';
+	this.spaceName = 'replanadu';
+	this._setSpaceName();
+}
+
+TwiddlersCount.prototype.go = function() {
+	var title =  $('#title').text();
+	var tag = '@' + this.spaceName;
+	this._search(title, tag);
+};
+
+TwiddlersCount.prototype._setSpaceName = function() {
+	var domains = document.location.hostname.split('.');
+	if (domains.length > 1) {
+		this.spaceName = domains[0];
+	}
+}
+
+TwiddlersCount.prototype.addButton = function(count) {
+	var button = $("<button>Twiddlers(" + count + ")</button>");
+	$('#container').append(button);
 }
 
 TwiddlersCount.prototype._search = function(title, tag) {
+	var context = this;
 	var success = function(data, status, xhr) {
 		console.log(data);
+		context.addButton(data.length);
 	};
 	$.getJSON('/search.json?q=title:"' + title + '" tag:' + tag, success, this._ajaxError);
 };
@@ -13,3 +35,7 @@ TwiddlersCount.prototype._search = function(title, tag) {
 TwiddlersCount.prototype._ajaxError = function(xhr, err, exc) {
 	console.log('Error');
 };
+
+$(document).ready(function () {
+	new TwiddlersCount().go();
+});
