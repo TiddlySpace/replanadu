a96282e75d6c21f3dde9f493f6c65cc63555da01
Chris Dent <chris.dent@gmail.com>
2013-08-23 14:57:06 +0100

    retrieve the local tiddler and display it

--- assets/twiddlers.js
+++ assets/twiddlers.js
@@ -8,6 +8,7 @@ function Twiddlers() {
 Twiddlers.prototype.getTwiddlers = function() {
 	var tag = '@' + this.spaceName;
 	this._search(this.title, tag);	
+	this._getLocalTiddler(this.title);
 };
 
 Twiddlers.prototype._init = function() {
@@ -17,6 +18,30 @@ Twiddlers.prototype._init = function() {
 Twiddlers.prototype._setTitle = function() {
 	this.title = document.location.hash.substring(1);
 };
+
+Twiddlers.prototype._displayTiddler = function(tiddlerData, place) {
+	place.html(tiddlerData.render);
+	place.prepend('<h1>' + tiddlerData.title + '</h1>');
+};
+
+Twiddlers.prototype._getLocalTiddler = function(title) {
+	var context = this,
+		place = $('#local');
+
+	var success = function(data) {
+		context._displayTiddler(data, place);
+	};
+
+	$.ajax({
+	    url: '/' + encodeURIComponent(title) + '?render=1',
+        type: 'GET', 
+        success: success,
+        error: this._ajaxError,
+        contentType: 'application/json',
+        dataType: 'json'	
+	});
+};
+
 
 Twiddlers.prototype._search = function(title, tag) {
 	var context = this;
