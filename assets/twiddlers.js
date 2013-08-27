f0832ef116fa5dc32d0692855b4a45242db6c1be
Ben Paddock <pads@thisispads.me.uk>
2013-08-27 13:38:36 +0100

    Reformat code - indent at 4 spaces

--- assets/twiddlers.js
+++ assets/twiddlers.js
@@ -1,139 +1,138 @@
-
 function Twiddlers() {
-	this.title = undefined;
-	this.currentUser = tiddlyweb.status.username;
-	this._init();
-	this.titleTemplate = this._getTemplate('#tiddler-title-template');
-	this.tiddlerTemplate = this._getTemplate('#tiddler-template');
-	this.listTemplate = this._getTemplate('#related-list-template');
-	this.followers = [];
+    this.title = undefined;
+    this.currentUser = tiddlyweb.status.username;
+    this._init();
+    this.titleTemplate = this._getTemplate('#tiddler-title-template');
+    this.tiddlerTemplate = this._getTemplate('#tiddler-template');
+    this.listTemplate = this._getTemplate('#related-list-template');
+    this.followers = [];
 }
 
-Twiddlers.prototype._init = function() {
-	this._setTitle();
+Twiddlers.prototype._init = function () {
+    this._setTitle();
     this.getTwiddlers();
 };
 
-Twiddlers.prototype._getTemplate = function(id) {
-	return Handlebars.compile($(id).html());		
+Twiddlers.prototype._getTemplate = function (id) {
+    return Handlebars.compile($(id).html());
 };
 
-Twiddlers.prototype.getTwiddlers = function() {
-	this._allSearch(this.title);	
-	this._getLocalTiddler(this.title);
+Twiddlers.prototype.getTwiddlers = function () {
+    this._allSearch(this.title);
+    this._getLocalTiddler(this.title);
 };
 
-Twiddlers.prototype._setTitle = function() {
-	this.title = document.location.hash.substring(1);
+Twiddlers.prototype._setTitle = function () {
+    this.title = document.location.hash.substring(1);
 };
 
-Twiddlers.prototype._displayRelated = function(tiddlers) {
+Twiddlers.prototype._displayRelated = function (tiddlers) {
     var list = $('#relatedlist');
     list.empty();
     list.append(this.listTemplate({ tiddlers: tiddlers }));
 };
 
-Twiddlers.prototype._displayTiddler = function(tiddler) {
-	$('header').html(this.titleTemplate(tiddler));
-	$('#local').html(this.tiddlerTemplate({ title: tiddler.title, html: tiddler.render }))
+Twiddlers.prototype._displayTiddler = function (tiddler) {
+    $('header').html(this.titleTemplate(tiddler));
+    $('#local').html(this.tiddlerTemplate({ title: tiddler.title, html: tiddler.render }))
 };
 
-Twiddlers.prototype._getLocalTiddler = function(title) {
-	var context = this;
-	var success = function(data) {
-		context._displayTiddler(data);
-	};
-	this._getTiddler(title, success);
+Twiddlers.prototype._getLocalTiddler = function (title) {
+    var context = this;
+    var success = function (data) {
+        context._displayTiddler(data);
+    };
+    this._getTiddler(title, success);
 };
 
-Twiddlers.prototype.loadTiddler = function(uri, elem) {
-	var success = function(data) {
-		$(elem).html(data.render);
-	};
+Twiddlers.prototype.loadTiddler = function (uri, elem) {
+    var success = function (data) {
+        $(elem).html(data.render);
+    };
     var match = 'tiddlyspace.com/';
     uri = uri.substring(uri.indexOf(match) + match.length) + '?render=1';
     this._doGET(uri, success, this._ajaxError);
 };
 
-Twiddlers.prototype._getTiddler = function(title, success) {
-	this._doGET('/' + encodeURIComponent(title) + '?render=1', success, this._ajaxError);
+Twiddlers.prototype._getTiddler = function (title, success) {
+    this._doGET('/' + encodeURIComponent(title) + '?render=1', success, this._ajaxError);
 };
 
 // XXX: somewhat dupe from TwiddlersCount!
 // Only do this if the currentUser is not GUEST
-Twiddlers.prototype._getFollowers = function() {
-	if (this.followers.length == 0) {
-		var context = this;
-		var success = function(data, status, xhr) {
-			var followers = $.trim(data).split('\n');
-			context.followers = $.map(followers, function(item) {
-				return item.replace(/^@/, '');
-			});
-			context._followSearch(context.followers);
-		};
-		var url = '/search.txt?q=bag:' + this.currentUser
-			+ '_public%20tag:follow'
-			+ '%20_limit:999';
-		this._doGET(url, success, this._ajaxError);
-	}
-	this._followSearch(this.followers);
+Twiddlers.prototype._getFollowers = function () {
+    if (this.followers.length == 0) {
+        var context = this;
+        var success = function (data, status, xhr) {
+            var followers = $.trim(data).split('\n');
+            context.followers = $.map(followers, function (item) {
+                return item.replace(/^@/, '');
+            });
+            context._followSearch(context.followers);
+        };
+        var url = '/search.txt?q=bag:' + this.currentUser
+            + '_public%20tag:follow'
+            + '%20_limit:999';
+        this._doGET(url, success, this._ajaxError);
+    }
+    this._followSearch(this.followers);
 };
 
 
-Twiddlers.prototype._search = function(url) {
-	var context = this;
-	var success = function(data, status, xhr) {
-		context._displayRelated(data);
-	};
-	this._doGET(url, success, this._ajaxError);
+Twiddlers.prototype._search = function (url) {
+    var context = this;
+    var success = function (data, status, xhr) {
+        context._displayRelated(data);
+    };
+    this._doGET(url, success, this._ajaxError);
 };
 
-Twiddlers.prototype._allSearch = function(title) {
-	var url = '/search.json?q=title:"' + encodeURIComponent(title) + '"';
-	this._search(url);
+Twiddlers.prototype._allSearch = function (title) {
+    var url = '/search.json?q=title:"' + encodeURIComponent(title) + '"';
+    this._search(url);
 }
 
-Twiddlers.prototype._followSearch = function(followers) {
-	var url = '/search?q=title:"' + encodeURIComponent(this.title) +
-		'"%20' + 'modifier:' + followers.join('%20OR%20modifier:');
-	this._search(url);
+Twiddlers.prototype._followSearch = function (followers) {
+    var url = '/search?q=title:"' + encodeURIComponent(this.title) +
+        '"%20' + 'modifier:' + followers.join('%20OR%20modifier:');
+    this._search(url);
 }
 
-Twiddlers.prototype._doGET = function(url, success, error) {
-	$.ajax({
-	    url: url,
-        type: 'GET', 
+Twiddlers.prototype._doGET = function (url, success, error) {
+    $.ajax({
+        url: url,
+        type: 'GET',
         success: success,
         error: error,
         headers: { 'X-ControlView': 'false' },
         contentType: 'application/json',
-        dataType: 'json'	
-	});	
+        dataType: 'json'
+    });
 };
 
 var app = undefined;
 $(document).ready(function () {
     app = new Twiddlers();
-	Handlebars.registerHelper('get_space_name', function(context, options) {
-	    return context.split('_')[0];
-	});	
-	Handlebars.registerHelper('get_site_icon_uri', function(context, options) {
-	    return 'http://' + context + '.tiddlyspace.com/SiteIcon';
-	});		
-	$(document).on('click', '.tiddler-button', function() {
+    Handlebars.registerHelper('get_space_name', function (context, options) {
+        return context.split('_')[0];
+    });
+    Handlebars.registerHelper('get_site_icon_uri', function (context, options) {
+        return 'http://' + context + '.tiddlyspace.com/SiteIcon';
+    });
+    $(document).on('click', '.tiddler-button', function () {
         var $button = $(this);
         var $article = $button.next();
-		var uri = $button.data('uri');
-        if($button.hasClass('open')) {
+        var uri = $button.data('uri');
+        if ($button.hasClass('open')) {
             $button.removeClass('open');
             $article.hide();
         } else {
             $button.addClass('open');
-            if($article.children().length === 0) {
-		        app.loadTiddler(uri, $button.parent().children('article'));
+            if ($article.children().length === 0) {
+                app.loadTiddler(uri, $button.parent().children('article'));
             } else {
                 $article.show();
             }
         }
-	});
+    });
 });
--- assets/twiddlerscount.js
+++ assets/twiddlerscount.js
@@ -1,124 +1,124 @@
 (function () {
-	"use strict";
+    "use strict";
 
-	var allImage = '/bags/replanadu_public/tiddlers/replanadu-white.png';
-	var followImage = '/bags/replanadu_public/tiddlers/replanadu-gold.png';
+    var allImage = '/bags/replanadu_public/tiddlers/replanadu-white.png';
+    var followImage = '/bags/replanadu_public/tiddlers/replanadu-gold.png';
 
-	function TwiddlersCount() {
-		this.baseUrl = '';
-		this.title = undefined;
-		this.currentUser = tiddlyweb.status.username;
-		this.targetURI = window.location.href.match(
-			tiddlyweb.status.server_host.host)
-				? 'replanadu' : 'replanadu.html';
-		this._init();
-	}
+    function TwiddlersCount() {
+        this.baseUrl = '';
+        this.title = undefined;
+        this.currentUser = tiddlyweb.status.username;
+        this.targetURI = window.location.href.match(
+            tiddlyweb.status.server_host.host)
+            ? 'replanadu' : 'replanadu.html';
+        this._init();
+    }
 
-	TwiddlersCount.prototype.search = function() {
-		this._getFollowers();
-	};
+    TwiddlersCount.prototype.search = function () {
+        this._getFollowers();
+    };
 
-	TwiddlersCount.prototype._init = function() {
-		this.title = $('#title').text();
-	};
+    TwiddlersCount.prototype._init = function () {
+        this.title = $('#title').text();
+    };
 
-	TwiddlersCount.prototype._getFollowers = function() {
-		if (this.currentUser !== 'GUEST') {
-			var context = this;
-			var success = function(data, status, xhr) {
-				var followers = $.trim(data).split('\n');
-				followers = $.map(followers, function(item) {
-					return item.replace(/^@/, '');
-				});
-				context._search(followers);
-			};
-			var url = '/search.txt?q=bag:' + this.currentUser
-				+ '_public%20tag:follow'
-				+ '%20_limit:999';
-			this._doGET(url, success, this._ajaxError);
-		} else {
-			this._search([]);
-		}
-	};
+    TwiddlersCount.prototype._getFollowers = function () {
+        if (this.currentUser !== 'GUEST') {
+            var context = this;
+            var success = function (data, status, xhr) {
+                var followers = $.trim(data).split('\n');
+                followers = $.map(followers, function (item) {
+                    return item.replace(/^@/, '');
+                });
+                context._search(followers);
+            };
+            var url = '/search.txt?q=bag:' + this.currentUser
+                + '_public%20tag:follow'
+                + '%20_limit:999';
+            this._doGET(url, success, this._ajaxError);
+        } else {
+            this._search([]);
+        }
+    };
 
-	TwiddlersCount.prototype.addButton = function(value) {
-		var img = $('<img>');
-		var button = $('<a>');
-		img.attr({
-			src: allImage,
-			alt: 'twinned tiddlers'
-		});
-		button.attr('id', 'twiddlers');
-		button.attr('target', '_blank');
-		button.attr('data-twiddlerall', value);
-		button.attr('href', '/' + this.targetURI + '#'
-				+ encodeURIComponent(this.title));
-		button.addClass('twiddlers');
-		button.addClass('twiddlerall');
-		// using CSS becuase we are a remote include widget
-		button.css({
-			position: 'absolute',
-			border: 0,
-			background: 'transparent',
-			top: '1px',
-			right: '80px',
-			opacity: 0.5
-		});
-		// XXX: set opacity on hover
+    TwiddlersCount.prototype.addButton = function (value) {
+        var img = $('<img>');
+        var button = $('<a>');
+        img.attr({
+            src: allImage,
+            alt: 'twinned tiddlers'
+        });
+        button.attr('id', 'twiddlers');
+        button.attr('target', '_blank');
+        button.attr('data-twiddlerall', value);
+        button.attr('href', '/' + this.targetURI + '#'
+            + encodeURIComponent(this.title));
+        button.addClass('twiddlers');
+        button.addClass('twiddlerall');
+        // using CSS becuase we are a remote include widget
+        button.css({
+            position: 'absolute',
+            border: 0,
+            background: 'transparent',
+            top: '1px',
+            right: '80px',
+            opacity: 0.5
+        });
+        // XXX: set opacity on hover
 
-		button.append(img);
-		$('#container').append(button);
-	};
+        button.append(img);
+        $('#container').append(button);
+    };
 
-	TwiddlersCount.prototype.updateButton = function(value) {
-		var button = $('#twiddlers');
-		var img = button.find('img');
-		button.attr('data-tiddlerfollow', value);
-		button.addClass('twiddlerfollow');
-		img.attr('src', followImage);
-	};
+    TwiddlersCount.prototype.updateButton = function (value) {
+        var button = $('#twiddlers');
+        var img = button.find('img');
+        button.attr('data-tiddlerfollow', value);
+        button.addClass('twiddlerfollow');
+        img.attr('src', followImage);
+    };
 
-	TwiddlersCount.prototype._followSearch = function(followers) {
-		var context = this;
-		var followSuccess = function(data, status, xhr) {
-			context.updateButton($.trim(data).split('\n').length);
-		};
-		var followUrl = '/search.txt?q=title:"' + encodeURIComponent(this.title) +
-			'"%20' + 'modifier:' + followers.join('%20OR%20modifier:');
-		this._doGET(followUrl, followSuccess, this._ajaxError);
-	};
+    TwiddlersCount.prototype._followSearch = function (followers) {
+        var context = this;
+        var followSuccess = function (data, status, xhr) {
+            context.updateButton($.trim(data).split('\n').length);
+        };
+        var followUrl = '/search.txt?q=title:"' + encodeURIComponent(this.title) +
+            '"%20' + 'modifier:' + followers.join('%20OR%20modifier:');
+        this._doGET(followUrl, followSuccess, this._ajaxError);
+    };
 
-	TwiddlersCount.prototype._search = function(followers) {
-		var context = this;
-		var allSuccess = function(data, status, xhr) {
-			context.addButton($.trim(data).split('\n').length);
-			if (followers.length > 0) {
-				context._followSearch(followers);
-			}
-		};
-		var allUrl = '/search.txt?q=title:"' + encodeURIComponent(this.title) +
-			'"';
-		this._doGET(allUrl, allSuccess, this._ajaxError);
-	};
+    TwiddlersCount.prototype._search = function (followers) {
+        var context = this;
+        var allSuccess = function (data, status, xhr) {
+            context.addButton($.trim(data).split('\n').length);
+            if (followers.length > 0) {
+                context._followSearch(followers);
+            }
+        };
+        var allUrl = '/search.txt?q=title:"' + encodeURIComponent(this.title) +
+            '"';
+        this._doGET(allUrl, allSuccess, this._ajaxError);
+    };
 
-	TwiddlersCount.prototype._doGET = function(url, success, error) {
-		$.ajax({
-			url: url,
-			type: 'GET',
-			success: success,
-			error: error,
-			headers: { 'X-ControlView': 'false' },
-			contentType: 'text/plain',
-			dataType: 'text'
-		});
-	};
+    TwiddlersCount.prototype._doGET = function (url, success, error) {
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: success,
+            error: error,
+            headers: { 'X-ControlView': 'false' },
+            contentType: 'text/plain',
+            dataType: 'text'
+        });
+    };
 
-	TwiddlersCount.prototype._ajaxError = function(xhr, err, exc) {
-		console.log('Error', err, exc);
-	};
+    TwiddlersCount.prototype._ajaxError = function (xhr, err, exc) {
+        console.log('Error', err, exc);
+    };
 
-	$(document).ready(function () {
-		new TwiddlersCount().search();
-	});
+    $(document).ready(function () {
+        new TwiddlersCount().search();
+    });
 
 }());
